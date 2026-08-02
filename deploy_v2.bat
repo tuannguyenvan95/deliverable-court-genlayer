6eeb6f551de5007b3b1a5ace50079c09282b068f
Admin User <admin@example.com>
2026-08-02 15:34:15 +0700

    chore: update deploy_v2.bat with accurate contract address and valid vercel deploy syntax

--- deploy_v2.bat
+++ deploy_v2.bat
@@ -3,4 +3,4 @@ git add .
 git commit -m "Stunning UI Overhaul"
 git push
 cd frontend
-npx vercel deploy --prod --yes --env VITE_CONTRACT_ADDRESS=0x526C759F9735306714fff9c95EB16B02E0875fEF
+npx.cmd vercel deploy --prod --yes --env VITE_CONTRACT_ADDRESS=0xb90894A6E43093aD737231243D2e6121160CbD15
